5b4094bec5259fbf49906b4d3bffa05524645df1
Michał Pańczyk <mjpanczyk@gmail.com>
2018-05-27 19:03:39 +0200

    Add parsing and fetching logic

--- fetch_books/fetch_books/spiders/packtpub.py
+++ fetch_books/fetch_books/spiders/packtpub.py
@@ -1,11 +1,77 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from os import environ
+import os
+import pathlib
 
 class PacktpubSpider(scrapy.Spider):
     name = 'packtpub'
     allowed_domains = ['packtpub.com']
-    start_urls = ['http://packtpub.com/']
+    base_url = 'https://www.packtpub.com/'
+    start_urls = [base_url]
+    storage_dir = './books'
+
+    email = environ['PACKT_EMAIL']
+    password = environ['PACKT_PASSWORD']
 
     def parse(self, response):
-        pass
+        yield scrapy.FormRequest.from_response(
+            response,
+            formcss='#packt-user-login-form',
+            formdata={
+              'email': self.email,
+              'password': self.password,
+            },
+            callback=self.parse_loggedin,
+        )
+
+    def parse_loggedin(self, response):
+        yield scrapy.Request(
+            url='https://www.packtpub.com/account/my-ebooks',
+            callback=self.parse_list,
+        )
+
+    def parse_list(self, response):
+        for item in response.css('#product-account-list > div'):
+            title = item.css('div.product-top-line > div.float-left.content-width > div.title ::text').extract_first().strip()
+            title = title.replace(' [eBook]', '')
+            slug = title.replace(' ', '_').lower()
+            path = pathlib.Path(self.storage_dir + os.path.sep + slug)
+            authors = item.css('div.product-top-line > div.float-left.content-width > div.author ::text').extract_first().strip()
+            path.mkdir(parents=True, exist_ok=True)
+            path.joinpath('authors.txt').write_text(authors)
+            path.joinpath('title.txt').write_text(title)
+
+            pdf_link = item.css('div.product-buttons-line.toggle > div:nth-child(2) > a:nth-child(1) ::attr(href)').extract_first()
+            yield scrapy.Request(
+                url=self.base_url + pdf_link,
+                callback=self.save_file,
+                meta={'path': path.joinpath(slug + '.pdf')},
+            )
+            epub_link = item.css('div.product-buttons-line.toggle > div:nth-child(2) > a:nth-child(2) ::attr(href)').extract_first()
+            yield scrapy.Request(
+                url=self.base_url + epub_link,
+                callback=self.save_file,
+                meta={'path': path.joinpath(slug + '.epub')},
+            )
+            mobi_link = item.css('div.product-buttons-line.toggle > div:nth-child(2) > a:nth-child(3) ::attr(href)').extract_first()
+            yield scrapy.Request(
+                url=self.base_url + mobi_link,
+                callback=self.save_file,
+                meta={'path': path.joinpath(slug + '.mobi')},
+            )
+            code_link = item.css('div.product-buttons-line.toggle > div:nth-child(2) > a:nth-child(4) ::attr(href)').extract_first()
+            yield scrapy.Request(
+                url=self.base_url + pdf_link,
+                callback=self.save_file,
+                meta={'path': path.joinpath('codes' + '.zip')},
+            )
+
+            isbn = item.css('div.product-buttons-line.toggle > div:nth-child(1) > a:nth-child(1) > div ::attr(isbn)').extract_first()
+            path.joinpath('isbn.txt').write_text(isbn)
+
+    def save_file(self, response):
+        path = response.meta['path']
+        self.logger.info('writing %s', str(path))
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.write_bytes(response.body)
